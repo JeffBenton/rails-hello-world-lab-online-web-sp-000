e95fba9478459545b609f42c450251711bd12fce
Jeff Benton <sikletz@gmail.com>
2019-05-28 21:22:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,3 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
-https://learn.co/tracks/full-stack-web-development-v7/rails/introduction-to-rails/rails-hello-world#
